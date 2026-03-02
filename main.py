88241115833aedc0e95a176aad76b694bd69a47a
Haker <noyemkaren@gmail.com>
2026-03-02 14:42:08 +0300

    Math

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import requests
+from fastapi import FastAPI, Request
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+formul = ["S = п*r²","C = п*d", "S=a*b", "P=a2+b2", "P = 2πr"]
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {
+            "request": request,
+            "so": formul[0],
+            "co": formul[1],
+            "po": formul[4]
+        })
+
+@app.get("/fSO")
+async def fso(a: float):
+    result = a * a * 3.14
+    return {"answer": round(result, 2)}  # Округляем до 2 знаков
+
+@app.get("/fCO")
+async def fco(a: float):
+    result = a * 3.14
+    return {"answer": round(result, 2)}
+
+@app.get("/fPO")
+async def fpo(a: float):
+    result = 2* 3.14 * a
+    return {"answer": round(result, 2)} #p=2пr
+
+@app.get("/test")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "test.html", {
+            "request": request,
+        })
+
+
+
+
